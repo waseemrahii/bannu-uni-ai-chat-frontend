800b05c35c63299fb6b85757caa0d3931c84366c
Wasim <princewasim971@gmail.com>
2025-11-01 15:10:32 +0500

    remove base file

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
     <ErrorBoundary>
       <AuthProvider>
-        <Router basename="/rag">
+        <Router>
           <Layout>
             <Routes>
               <Route path="/" element={<Home />} />
